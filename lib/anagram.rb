55e854d61c909a3d0ab28c91a289d6dc489eaf73
Kathy Hudson <khitx@icloud.com>
2018-01-28 21:54:26 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
 
   def match(pos_anagrams)
     #binding.pry
-    pos_anagrams.collect { |p_a| p_a if p_a.split(//).sort == @word.split(//).sort }.flatten
+    pos_anagrams.collect.flatten { |p_a| p_a if p_a.split(//).sort == @word.split(//).sort }
     #pos_anagrams.each { |p_a| p_a.split(/./).sort == @word.split(/./).sort }
   end
 end
