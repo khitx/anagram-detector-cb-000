94ed4314c3598e48f04d28fc6e7855d7ef51bac8
Kathy Hudson <khitx@icloud.com>
2018-01-28 21:27:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @@word = word
+  end
+
+  def match(pos_anagrams)
+    pos_anagrams.collect { |p_a| p_a.sort == @word.sort  }
+  end
+end
